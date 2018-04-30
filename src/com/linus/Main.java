6a8c9ed70cd390a2ed066be42642fb620015844f
Linus Töbke <linustobke@Linuss-MacBook-Pro.local>
2018-04-30 16:23:21 +0200

    Initial commit

---
+++ src/com/linus/Main.java
@@ -0,0 +1,11 @@
+package com.linus;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            String halloWelt = "Hallo Welt";
+        System.out.println(halloWelt);
+
+    }
+}
